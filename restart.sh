9dc29ef1929421a48a762290189467a007651ed5
GurovSD <gurov.sd@gmail.com>
2021-11-23 16:37:01 +0300

    docker initialization script added to restart.sh

--- restart.sh
+++ restart.sh
@@ -3,3 +3,13 @@ set -e
 
 (docker stop pg_jobers && docker rm pg_jobers) || true
 sudo rm -rf /home/laooglee/gb/pg/data
+
+docker run \
+    -d \
+    -p 5432:5432 \
+    --name pg_jobers \
+    -e POSTGRES_PASSWORD=password \
+    -e PGDATA=/var/lib/postgresql/data \
+    -v /home/laooglee/gb/pg/data:/var/lib/postgresql/data \
+    -v /home/laooglee/gb/pg/init:/docker-entrypoint-init.d \
+    postgres:14
